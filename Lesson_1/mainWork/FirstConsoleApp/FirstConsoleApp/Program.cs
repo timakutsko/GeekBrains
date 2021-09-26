005646b4e692f22e644cffd446606a7b476594f6
Tsimafei <tima.kutsko@gmail.com>
2021-09-26 14:24:41 +0300

    Основная задача

---
+++ Lesson_1/mainWork/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // по условиям вывести имя пользователя без начального ввода. Это будет так
+            Console.WriteLine($"Привет, {Environment.UserName}, сегодня {DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year}");
+            // а вот сохранить имя пользователя из консоли - я понял так
+            var inputUser = Console.ReadLine();
+            Console.WriteLine($"Привет, {inputUser}, сегодня {DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year}");
+        }
+    }
+}
