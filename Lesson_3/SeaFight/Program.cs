11c9f84e723834327cc49682a1aeb5b272e6e0f1
Tsimafei <tima.kutsko@gmail.com>
2021-10-02 19:47:52 +0300

    Морской бой

---
+++ Lesson_3/SeaFight/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SeaFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seaArea = new char[10, 10]
+            {
+                {'O', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X' },
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' },
+                {'O', 'O', 'X', 'X', 'X', 'O', 'O', 'O', 'O', 'O' },
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O' },
+                {'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O' },
+                {'O', 'O', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'O' },
+                {'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O' },
+                {'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'O' },
+                {'O', 'O', 'O', 'O', 'X', 'X', 'X', 'O', 'O', 'O' },
+                {'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' },
+            };
+            int rows = seaArea.GetUpperBound(0) + 1;
+            int colums = seaArea.Length / rows;
+            for(int i=0; i<rows; i++)
+            {
+                for(int j=0; j<colums; j++)
+                {
+                    Console.Write($"{seaArea[i,j]} ");
+                }
+                Console.WriteLine();   
+            }
+        }
+    }
+}
