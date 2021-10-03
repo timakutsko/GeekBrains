d162ce0c51d2a739054d242a64b0d3862c76ac44
Tsimafei <tima.kutsko@gmail.com>
2021-10-03 19:34:15 +0300

    Сумма чисел

---
+++ Lesson_4/NumbSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumbSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void MySum(string usStr)
+            {
+                int sum = 0;
+                var chArr = usStr.Split(" ");
+                foreach(string curChar in chArr)
+                {
+                    if (int.TryParse(curChar, out int numb))
+                    {
+                        sum += numb;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Только числа!");
+                        Environment.Exit(0);
+                    }
+                }
+                Console.WriteLine($"Сумма: {sum}");
+            }
+            Console.Write("Введи числа разделенные пробелом: ");
+            var usInput = Console.ReadLine();
+            MySum(usInput);
+        }
+    }
+}
