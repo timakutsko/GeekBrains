56cea4905ec7398d8bd080cbf3db84da302ec8d9
Tsimafei <tima.kutsko@gmail.com>
2021-10-10 15:24:46 +0300

    Сохранение в файл

---
+++ Lesson_5/Saver/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Saver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите данные для записи: ");
+            File.WriteAllText("saver.txt", Console.ReadLine());
+        }
+    }
+}
