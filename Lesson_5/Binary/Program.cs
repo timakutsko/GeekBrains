f24285aa66fc97c56df0b9b9c10bdf6db895a2fc
Tsimafei <tima.kutsko@gmail.com>
2021-10-10 17:30:17 +0300

    Запись бинарных данных

---
+++ Lesson_5/Binary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи числа в диапозоне от 0 до 255: ");
+            string[] strArr_1 = Console.ReadLine().Trim().Split(" ");
+            byte[] byteArr = new byte [strArr_1.Length];
+            for(int i = 0; i < strArr_1.Length; i++)
+            {
+                byteArr[i] = Convert.ToByte(strArr_1[i]);
+            }
+            File.WriteAllBytes("bytes.bin", byteArr);
+        }
+    }
+}
