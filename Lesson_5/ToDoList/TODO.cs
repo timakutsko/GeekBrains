0ba704e189edbaaa7c7a725bb79ce94efe854049
Tsimafei <tima.kutsko@gmail.com>
2021-10-11 22:40:48 +0300

    TODO лист
    Можно было больше методов использовать - но уже слишком поздно)

---
+++ Lesson_5/ToDoList/TODO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class TODO
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public string UserView(bool isTrue)
+        {
+            if (isTrue)
+            {
+                return "X";
+            }
+            else 
+            {
+                return " "; 
+            }
+        }
+    }
+}
---
+++ Lesson_5/ToDoList/TODOList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ToDoList
+{
+    class TODOList
+    {
+        public List<TODO> Items { get; set; }
+    }
+}
