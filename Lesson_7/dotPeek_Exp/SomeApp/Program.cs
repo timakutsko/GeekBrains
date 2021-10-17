48dcc7150092523b0f997e3e9b3caee1931702a4
Tsimafei <tima.kutsko@gmail.com>
2021-10-17 22:38:16 +0300

    Все для урока №7 (сборки)

---
+++ Lesson_7/dotPeek_Exp/SomeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MSBuild.Program
+// Assembly: SomeApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 731679EC-BE7D-4B45-9157-428ED26C4E78
+// Assembly location: E:\GeekBrains\Lesson_7\SomeApp\bin\Debug\net5.0\SomeApp.dll
+
+using System;
+
+namespace MSBuild
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.Write("Введи число, для пошагового двукратного увеличения: ");
+      int int32 = Convert.ToInt32(Console.ReadLine());
+      for (int index = 1; index < 6; ++index)
+        Console.WriteLine(string.Format("Шаг №{0}, результат: {1}", (object) index, (object) (int32 *= 2)));
+    }
+  }
+}
---
+++ Lesson_7/dotPeek_Exp/ex/Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: HelloWorld.Program
+// Assembly: SomeApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 5FE392E5-FEAA-4380-BA9E-354F5959F23D
+// Assembly location: E:\GeekBrains\Lesson_7\ILDasm\ex.exe
+
+using System;
+
+namespace HelloWorld
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      string str = "some secret password";
+      Console.WriteLine("Enter password:");
+      if (!(Console.ReadLine() == str))
+        return;
+      Console.WriteLine("Welcome!");
+    }
+  }
+}
