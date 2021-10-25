190dcd26814b25afdc01f234df83fdd15e79df76
Tsimafei <tima.kutsko@gmail.com>
2021-10-25 23:34:15 +0300

    Практическое задание

---
+++ Lesson_9/FileManager/UserExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class UserExceptions : Exception
+    {
+        public UserExceptions(string discr, string userInput) : base(String.Format("{0}{1}", discr, userInput)) 
+        { 
+            // Логирование ошибок в файл .txt
+            File.AppendAllText("errors_exception.txt", $"{discr} {userInput}"); 
+            File.AppendAllText("errors_exception.txt", $"\n"); 
+        } 
+    }
+}
